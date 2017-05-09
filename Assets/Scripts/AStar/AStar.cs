f9b9f184f6f069e880e26030e109c53899edfb5b
Stanley Ung <stanleyung167@gmail.com>
2017-05-08 23:08:09 -0700

    AStar algorithm started

---
+++ Assets/Scripts/AStar/AStar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStar {
+
+   // Dictionary of tiles with a Point key and Node value
+   private static Dictionary<Point, Node> nodes;
+   
+   private static void CreateNodes() {
+      nodes = new Dictionary<Point, Node>();
+
+      // Iterate through each TileScript in the game and create a node for each
+      foreach (TileScript t in LevelManager.Instance.Tiles.Values) {
+         nodes.Add(t.GridPos, new Node(t));
+      }
+   }
+
+}
---
+++ Assets/Scripts/AStar/Node.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+   
+   // A point position of the tile
+   public Point GridPosition { get; private set; }
+
+   // A reference to the Tile's TileScript
+   public TileScript TileRef { get; private set; }
+
+   // Node constructor
+   public Node(TileScript t) {
+      this.TileRef = t;
+      this.GridPosition = t.GridPos;
+   }
+}
